95e00b22703860bb4af8a5e2a1eb098b0d67b607
列小狗 <3473427668@qq.com>
2026-01-11 13:16:34 +0800

    Cleanup: Remove binary files and build artifacts

--- CMakeFiles/my_tesla_app.dir/Math.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/my_tesla_app.dir/Math.cpp.o: /root/projects/learning/Math.cpp \
- /usr/include/stdc-predef.h /root/projects/learning/Math.h
--- CMakeFiles/tesla_Car.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tesla_Car.
-# This may be replaced when dependencies are built.
